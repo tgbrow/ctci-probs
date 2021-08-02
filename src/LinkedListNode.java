25590f3ca3855b1e683e6c37c2b8f4c0741113eb
Teddy <edward.teddy.brow@gmail.com>
2021-08-02 13:47:14 -0700

    Add soln for prob 2.1

--- src/LinkedListNode.java
+++ src/LinkedListNode.java
@@ -40,4 +40,30 @@ public class LinkedListNode {
         LinkedListNode head2 = new LinkedListNode(data, next2, null);
         return head2;
     }
+
+    // Note: The methods below are my additions (not from book).
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        LinkedListNode n = this;
+        while (n != null) {
+            sb.append(n.data);
+            if (n.next != null) {
+                sb.append(" > ");
+            }
+            n = n.next;
+        }
+        return sb.toString(); 
+    }
+
+    public static LinkedListNode buildSinglyLinkedList(int[] data) {
+        if (data.length < 1) return null;
+        LinkedListNode head = new LinkedListNode(data[0]);
+        LinkedListNode n = head;
+        for (int i = 1; i < data.length; ++i) {
+            n.next = new LinkedListNode(data[i]);
+            n = n.next;
+        }
+        return head;
+    }
 }
---
+++ src/Prob_02_01.java
@@ -0,0 +1,50 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Prob_02_01 {
+
+    // Note: Assume we're dealing with a *singly* linked list.
+
+    public static void main(String[] args) {
+        int[] testData = {1, 2, 3, 1, 5, 6, 1, 1, 9, 10, 5};
+        LinkedListNode list1 = LinkedListNode.buildSinglyLinkedList(testData);
+        LinkedListNode list2 = list1.clone();
+        System.out.println(list1);
+        removeDuplicates(list1);
+        System.out.println(list1);
+        removeDuplicatesBeta(list2);
+        System.out.println(list2);
+    }
+
+    private static void removeDuplicates(LinkedListNode n) {
+        Set<Integer> seen = new HashSet<>();
+        LinkedListNode prev = null;
+
+        while (n != null) {
+            if (!seen.add(n.data)) {
+                // Note: It's impossible for prev to be null for a duplicate.
+                prev.next = n.next;
+            } else { // n is not a duplicate
+                prev = n;
+            }
+            n = n.next;
+        }
+    }
+
+    // Version if we can't use aux data structure.
+    private static void removeDuplicatesBeta(LinkedListNode n) {
+        while (n != null) {
+            LinkedListNode runner = n;
+            while (runner.next != null) {
+                if (runner.next.data == n.data) {
+                    runner.next = runner.next.next;
+                } else {
+                    runner = runner.next;
+                }
+            }
+            n = n.next;
+        }
+    }
+    
+
+}
